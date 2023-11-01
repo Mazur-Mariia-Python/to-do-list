fc91906ab45e982dd6706021ab1b08a18b25b425
Mazur Mariya <mazurinka29@gmail.com>
2023-11-01 11:51:20 +0200

    Added to_do app

--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    """The theme of the task"""
+
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        """
+        Tags should be ordered by name
+        """
+
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    """Describes what you should do"""
+
+    content = models.CharField(max_length=255)
+    """Datetime, when a task was created"""
+    created_at = models.DateTimeField(auto_now_add=False)
+    """
+    Optional deadline datetime if a task should be done 
+    until some datetime
+    """
+    deadline = models.DateTimeField(auto_now_add=False, null=True)
+    """The boolean field that marks if the task is done or not"""
+    completed = models.BooleanField()
+    """Tags that are relevant for this task"""
+    tags = models.ManyToManyField(Tag, related_name="tags")
+
+    class Meta:
+        """
+        Tasks should be ordered from not done to done and from newest
+        to oldest
+        """
+
+        ordering = ["-created_at"] and ["completed"]
+
+    def __str__(self):
+        return self.content
